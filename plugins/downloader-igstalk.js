f917c8b179f87242e436ea70b648f6e02c06bac1
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-08 15:57:44 +0700

    Update downloader-igstalk.js

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,36 +1,28 @@
-import instaloader as drekkemren
-import shlex
-import sys
-import requests
-import os
-import time
-mr_dark = " ".join(map(shlex.quote, sys.argv[1:]))
-darkcoyy = drekkemren.Instaloader()
-profile = drekkemren.Profile.from_username(darkcoyy.context, mr_dark)
-params = {
-    'tkn': '125',
-    'd': '3000',
-    'u': f'https://gramhir.com/profile/{mr_dark}/{profile.userid}',
-    'fs': '0',
-    'w': '1280',
-    'h': '1280',
-    's': '100',
-    'z': '100',
-    'f': 'jpg',
-    'rt': 'jweb',
+import { instagramStalk } from '@bochilteam/scraper'
+
+let handler= async (m, { args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
+    const {
+        username,
+        name,
+        description,
+        followersH,
+        followingH,
+        postsH,
+    } = await instagramStalk(args[0])
+    m.reply(`
+${name} *(${username})*
+https://instagram.com/${username.replace(/^@/, '')}
+*${followersH}* Followers
+*${followingH}* Following
+*${postsH}* Posts
+*Bio:* ${description}
+`.trim())
 }
-print("Username: ", profile.username)
-print("User ID: ", profile.userid)
-print("Number of Posts: ", profile.mediacount)
-print("Followers: ", profile.followers)
-print("Followees: ", profile.followees)
-print("Bio: ", profile.biography,profile.external_url)
-print ("_© mikaz ~ MultiDevice 2023_")
-#os.system("rm ssweb.jpg")
-#response = requests.get('https://api.pikwy.com/', params=params).text
-#a = response.split('"iurl":"')[1];
-#b = a.split('"')[0];
-#print ('url screenshot: '+b)
-#response = requests.get(b)
-#with open("ssweb.jpg", "w") as f:
-#    f.write(response.text)
+
+handler.help = ['igstalk'].map(v => v + ' <username>')
+handler.tags = ['downloader']
+
+handler.command = /^(igstalk)$/i
+
+export default handler
