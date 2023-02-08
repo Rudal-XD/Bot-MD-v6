63f00effea3d03d89a8d7441a00524bcb75bc828
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-08 20:59:53 +0700

    Update downloader-igstalk.js

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,28 +1,38 @@
-import { instagramStalk } from '@bochilteam/scraper'
+let axios = require("axios")
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+response = args.join(' ')
+  if (!args[0]) throw 'User Name'
+  await conn.reply(m.chat, mess.wait, 0, { thumbnail: await(await fetch(ext.thum)).buffer(), contextInfo: {
+                  externalAdReply: {
+                    mediaUrl: 'https://youtu.be/-tKVN2mAKRI',
+                    title: ext.title,
+                    body: ext.body,
+                    thumbnail: await(await fetch(ext.thum)).buffer()
+                   }
+                 }
+               }
+           )
+	axios.get(`https://leyscoders-api.herokuapp.com/api/igstalk?user=${response}&apikey=demo`).then ((res) => {
+	 	let hasil = `Nama :${res.data.result.fullName}\nUser Name :${res.data.result.userame}\nBio :{res.data.result.biography}\nFollower :${res.data.result.follower}\nFollowing :${res.data.result.following}\nPostingan :${res.data.result.postsCount}`               
 
-let handler= async (m, { args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
-    const {
-        username,
-        name,
-        description,
-        followersH,
-        followingH,
-        postsH,
-    } = await instagramStalk(args[0])
-    m.reply(`
-${name} *(${username})*
-https://instagram.com/${username.replace(/^@/, '')}
-*${followersH}* Followers
-*${followingH}* Following
-*${postsH}* Posts
-*Bio:* ${description}
-`.trim())
+    conn.reply(m.chat, hasil, m)
+	})
 }
-
 handler.help = ['igstalk'].map(v => v + ' <username>')
-handler.tags = ['downloader']
-
+handler.tags = ['internet']
 handler.command = /^(igstalk)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
 
-export default handler
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
+
+module.exports = handler
