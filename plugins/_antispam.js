ff0a955512d310d1d3159d05564644a40a53e84d
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-02-09 12:40:38 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,23 +1,27 @@
-const { WAMessageStubType } = (await import('@adiwajshing/baileys')).default
-import { format } from 'util';
-
-let imgr = flaaa.getRandom()
-
-const isNumber = x => typeof x === 'number' && !isNaN(x)
-const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
-    clearTimeout(this)
-    resolve()
-}, ms))
-
 export async function all(m) {
-	if (m.fromMe && m.isBaileys) return !0
-	let text;
-	let setting = global.db.data.settings[this.user.jid]
-	if(!setting.anticall) return 
-	
-	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await conn.sendButton(m.chat, `👋 Kamu di Blockir karena menelepon *Bot*\n`,wm + '\n\n' + botdate, `${imgr + 'Anti Call'}`, [['OK','ok']], null)
-		await delay(1000)
-		await this.updateBlockStatus(m.chat, "block")
-	}
+    if (!m.message)
+        return
+    this.spam = this.spam ? this.spam : {}
+    let chat = global.db.data.chats[m.chat]
+    if (chat.antiSpam) {
+    if (m.sender in this.spam) {
+        this.spam[m.sender].count++
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+            if (this.spam[m.sender].count > 10) {
+               global.db.data.users[m.sender].banned = true
+                let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+    let caption = ` Banned *@${who.split("@")[0]}* Jangan spam kak!`
+    this.sendButton(m.chat, caption, wm, null, [['Matikan AntiSpam', '/disable antispam']], m, { mentions: this.parseMention(caption) })
+            }
+            this.spam[m.sender].count = 0
+            this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+        }
+    }
+    else
+        this.spam[m.sender] = {
+            jid: m.sender,
+            count: 0,
+            lastspam: 0
+        }
+     }
 }
